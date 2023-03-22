639c8f59b6b55170a77840ae25bc776a285fe156
esteban.bobbiesi <esteban.bobbiesi@kiuviservices.com.ar>
2023-03-22 00:52:58 -0300

    Added recipe API

--- app/core/models.py
+++ app/core/models.py
@@ -23,8 +23,6 @@ class UserManager(BaseUserManager):
 
     def create_superuser(self, email, password, **extra_fields):
         """Create and return a new superuser."""
-        if not email:
-            raise ValueError("Superuser must have an email address.")
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
